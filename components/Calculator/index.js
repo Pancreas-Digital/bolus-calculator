67fc752f203de2f30b497175b96ab7e42c211b39
Julián Prieto <herbie.juls@gmail.com>
2020-11-24 20:30:21 -0300

    Add Chakra UI

--- components/Calculator/index.js
+++ components/Calculator/index.js
@@ -1,28 +1,35 @@
 import { useState, useEffect } from 'react';
-import styles from './styles.module.scss';
+import {
+  Container,
+  Box,
+  FormControl,
+  FormLabel,
+  FormHelperText,
+  Input,
+  Slider,
+  SliderTrack,
+  SliderFilledTrack,
+  SliderThumb,
+  Button,
+  Text,
+  Stack,
+} from "@chakra-ui/react"
 
 export default function Calculator() {
   const [form, setForm] = useState({
-    minimo: '',
+    minimo: 0,
     glucemia: '',
     ratio: '',
     correction: '',
-    objective: '',
     amountOfCarbs: '',
   });
   const [result, setResult] = useState();
+
   /**
    * Runs on every refresh
    */
   useEffect(() => {
     console.log('form', form);
-    //   const valueInStorage = localStorage.getItem("form")
-
-    //   if (valueInStorage){
-    //     return
-    //   }
-    //   valueInStorage.setItem("form", JSON.stringify(form))
-    //
   }, [form]);
 
   /**
@@ -37,16 +44,17 @@ export default function Calculator() {
 
   const handleChange = (event) => {
     const { value, name } = event.target;
-
     setForm((prevState) => {
       const newForm = {
         ...prevState,
         [name]: Number(value),
       };
       localStorage.setItem('form', JSON.stringify(newForm));
+
       return newForm;
     });
   };
+
   const handleSubmit = (event) => {
     event.preventDefault();
     let res = form.amountOfCarbs / form.ratio;
@@ -57,36 +65,71 @@ export default function Calculator() {
     res = Math.round(res * 2) / 2;
     setResult(res);
   };
+
+  const handleSlider = value => {
+    setForm((prevState) => {
+      const newForm = {
+        ...prevState,
+        minimo: Number(value),
+      };
+      localStorage.setItem('form', JSON.stringify(newForm));
+
+      return newForm;
+    });
+  };
+
   return (
-    <div className={styles.component}>
-      <form onSubmit={handleSubmit}>
-        <label>
-          Minimo
-          <input value={form.minimo} name="minimo" onChange={handleChange} type="text" />
-        </label>
-        <label>
-          Glucemia
-          <input value={form.glucemia} name="glucemia" onChange={handleChange} type="text" />
-        </label>
-        <label>
-          Ratio
-          <input value={form.ratio} name="ratio" onChange={handleChange} type="text" />
-        </label>
-        <label>
-          Corrección
-          <input value={form.correction} name="correction" onChange={handleChange} type="text" />
-        </label>
-        <label>
-          Objetivo
-          <input value={form.objective} name="objective" onChange={handleChange} type="text" />
-        </label>
-        <label>
-          Cantidad de carbohidratos
-          <input value={form.amountOfCarbs} name="amountOfCarbs" onChange={handleChange} type="text" />
-        </label>
-        <button type="submit">Calcular</button>
-        <span>{result}</span>
-      </form>
-    </div>
+    <Container maxW="md">
+      <Box padding="4" bg="blue.50">
+        <form onSubmit={handleSubmit}>
+          <Stack spacing={5}>
+            <FormControl id="minimo">
+              <FormLabel>Mínimo: {form.minimo}</FormLabel>
+              <FormHelperText>We'll never share your email.</FormHelperText>
+              <Slider value={form.minimo} defaultValue={0} step={0.25} min={0} max={1} onChange={handleSlider}>
+                <SliderTrack>
+                  <SliderFilledTrack />
+                </SliderTrack>
+                <SliderThumb />
+              </Slider>
+            </FormControl>
+            <FormControl id="glucemia">
+              <FormLabel>Glucemia</FormLabel>
+              <FormHelperText>We'll never share your email.</FormHelperText>
+              <Input onChange={handleChange} type="number" name="glucemia" />
+            </FormControl>
+            <FormControl id="ratio">
+              <FormLabel>Ratio</FormLabel>
+              <FormHelperText>We'll never share your email.</FormHelperText>
+              <Input onChange={handleChange} type="number" name="ratio" />
+            </FormControl>
+            <FormControl id="correction">
+              <FormLabel>Corrección</FormLabel>
+              <FormHelperText>We'll never share your email.</FormHelperText>
+              <Input onChange={handleChange} type="number" name="correction" />
+            </FormControl>
+            <FormControl id="amountOfCarbs">
+              <FormLabel>Cantidad de carbohidratos</FormLabel>
+              <FormHelperText>We'll never share your email.</FormHelperText>
+              <Input onChange={handleChange} type="number" name="amountOfCarbs" />
+            </FormControl>
+            <Button
+              size="md"
+              height="48px"
+              width="200px"
+              border="2px"
+              colorScheme="blue"
+              type="submit"
+              style={{ margin: "24px auto" }}
+            >
+              Calcular
+            </Button>
+            <div style={{ display: 'flex', justifyContent: 'center' }}>
+              <Text fontSize="6xl">{result}</Text> 
+            </div>
+          </Stack>
+        </form>
+      </Box>
+    </Container>
   );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,16 @@
 import Head from 'next/head';
 import Calculator from '@components/Calculator';
+import { ChakraProvider } from "@chakra-ui/react"
 
 export default function Home() {
   return (
-    <div>
-      <Head>
-        <title>Calculadora V1</title>
-      </Head>
-      <Calculator />
-    </div>
+    <ChakraProvider>
+      <div>
+        <Head>
+          <title>Calculadora V1</title>
+        </Head>
+        <Calculator />
+      </div>
+    </ChakraProvider>
   );
 };
