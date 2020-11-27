ecdd31c658e9989bb6e7f3da6c6dd1bf9c12827e
Federico Motta <federico.motta@softvision.com>
2020-11-27 19:08:48 -0300

    Change title

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,13 @@
-import Head from "next/head";
-import Calculator from "@components/Calculator";
-import { ChakraProvider, Box } from "@chakra-ui/react";
+import Head from 'next/head';
+import Calculator from '@components/Calculator';
+import { ChakraProvider, Box } from '@chakra-ui/react';
 
 export default function Home() {
   return (
     <ChakraProvider>
       <div>
         <Head>
-          <title>Calculadora V1</title>
+          <title>Calculadora de bolos de insulina - Diabetes</title>
         </Head>
         <Box bg="blue.50" minHeight="100vh">
           <Calculator />
