291efb013043d134046a0d16a30975fa693a6ae2
Federico Motta <federico.motta@softvision.com>
2021-01-10 21:22:33 -0300

    Adding GA4

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,24 @@
-import Head from "next/head";
-import { Calculator } from "@components";
-import { ChakraProvider, Box } from "@chakra-ui/react";
+import Head from 'next/head';
+import { Calculator } from '@components';
+import { ChakraProvider, Box } from '@chakra-ui/react';
 
 export default function Home() {
   return (
     <ChakraProvider>
       <div>
         <Head>
+          <script async src="https://www.googletagmanager.com/gtag/js?id=G-XR9HVSZTNT" />
+
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-XR9HVSZTNT');
+        `,
+            }}
+          />
           <title>Calculadora de bolos de insulina - Diabetes</title>
         </Head>
         <Box bg="blue.50" minHeight="100vh">
